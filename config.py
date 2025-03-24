025c7d91bf0c17629adb6a7551967e7b27e2a8ba
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-03-24 14:04:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6489443094:AAFxg3ZVY7i0VKCbuJIre00SdTsJY5lEe7A")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7851602957:AAHnN0_34UpquufrtZrvwBZewPfo22PCnJo")
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "23990433"))
 #Your API Hash from my.telegram.org
